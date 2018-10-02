16807950436f1115c59b05583ad334e707d3dfac
ThibaultKeriven <thibault.keriven@devoteam.com>
2018-10-02 13:02:00 +0000

    added HelloWorld program

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        for(int i = 0; i != 5 ; i++){
+		System.out.println("Hello, World "+ i);
+	}
+    }
+
+}
