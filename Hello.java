ec53921f19573eedf39dc6fa45eb01007587492d
ThibaultKeriven <thibault.keriven@devoteam.com>
2018-10-03 12:56:25 +0000

    ...

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ public class Hello {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         for(int i = 0; i != 5 ; i++){
-		System.out.println("Hayoooooo, pioupiou World "+ i);
+		System.out.println("Hayoooooo, pioupioupioupioupiou World "+ i);
 	}
     }
 
